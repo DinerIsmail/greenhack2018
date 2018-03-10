3d476703671ccc6012965c0a8a8e98cb70bfdecf
Fantin Mesny <fm8@dhcp-10-240-143-15.cp.wireless.private.cam.ac.uk>
2018-03-10 17:11:54 +0000

    Moved files into separate folder

--- Website/exploring.js
+++ Website/exploring.js
@@ -13,27 +13,37 @@ $(document).ready(function() {
     reviewRef.on('value', function(snapshot) {
         var obj=snapshot.val();
         for(var key in obj) {
-            console.log(obj[key]['reviews']);
             var sumofratings=0;
             var sumofspans=0;
             for(var nb in obj[key]['reviews']) {
                 sumofratings+=obj[key]['reviews'][nb]['rating'];
                 sumofspans+=obj[key]['reviews'][nb]['lifespan'];
             };
-        var averagerating=sumofratings/obj[key]['reviews'].length;
-        var averagelifespan=sumofspans/obj[key]['reviews'].length;
-        dataSet.push([obj[key]['brand'],obj[key]['model'],key,averagerating,averagelifespan]);
+            var averagerating=sumofratings/obj[key]['reviews'].length;
+            var averagelifespan=sumofspans/obj[key]['reviews'].length;
+            var arr=[];
+            arr.push(obj[key]['brand'] || '');
+            arr.push(obj[key]['model'] || '');
+            arr.push(key || '');
+            arr.push(String(averagerating) || '');
+            arr.push(String(averagelifespan) || '');
+            dataSet.push(arr);
         };
+        
+        $('#table').DataTable( {
+            data: dataSet,
+            columns: [
+                { title: "Brand" },
+                { title: "Model" },
+                { title: "ASIN" },
+                { title: "Average rating" },
+                { title: "Average lifespan" }
+            ]
+        } );
     });
     console.log(dataSet);
-    $('#table').DataTable( {
-        data: dataSet,
-        columns: [
-            { title: "Brand" },
-            { title: "Model" },
-            { title: "ASIN" },
-            { title: "Average rating" },
-            { title: "Average lifespan" }
-        ]
-    } );
+    //for(var key in dataSet) {console.log(key)};
+    //var dataSet=[['test1','test2','test3','test4','test5'],['test1','test2','test3','test4','test5']]
+    //console.log(dataSet);
+    
 });
